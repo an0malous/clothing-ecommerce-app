d340766c0a4215cbeffba3db24a75544d3a1fec8
an0malous <anomalous58@gmail.com>
2020-10-16 00:02:29 +0900

    added hover and effects to components on homepage

--- src/components/menu-item/menu-item.component.js
+++ src/components/menu-item/menu-item.component.js
@@ -2,14 +2,17 @@ import React from 'react';
 import './menu-item.styles.scss'
 
 const MenuItem = ({ title, imageUrl, id, size }) => (
-    <div style={{
+    <div className={`${size} menu-item`}>
+        <div style={{
         backgroundImage: `url(${imageUrl})`
-        }} className={`${size} menu-item`}>
+        }} className="background-image"/>
         <div className="content">
-            <h1 className="title">{title}</h1>
+            <h1 className="title">{title.toUpperCase()}</h1>
             <span className="subtitle"></span>
         </div>
+    
     </div>
+    
 )
 
 export default MenuItem
